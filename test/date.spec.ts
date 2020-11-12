aa384dec35a28eedde7e0c567da5d783a5b24525
litt1e-p <free9fw@gmail.com>
2020-11-12 11:05:10 +0800

    test date

--- test/date.spec.ts
+++ test/date.spec.ts
@@ -1,37 +1,42 @@
 import { formatDate } from '../'
 
-test('object - objReverse', () => {
+function tEq(t: string) {
+  return +new Date(t)
+}
+
+test('date - formatDate', () => {
   expect(formatDate()).toEqual('');
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   expect(formatDate('abc')).toEqual('');
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   expect(formatDate('2020-11-04', 'Y/M/d')).toEqual('2020/11/4');
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   expect(formatDate('2020-11-04 1:08:12', 'Y/M/d')).toEqual('2020/11/4');
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   const date = new Date('Wed Nov 4 2020 12:06:02 GMT+0800 (中国标准时间)')
-  expect(formatDate(date)).toEqual('2020-11-04 12:06:02');
+  expect(tEq(formatDate(date))).toEqual(tEq('2020-11-04 12:06:02'));
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   const date = new Date('Wed Nov 4 2020 12:06:02 GMT+0800 (中国标准时间)')
-  expect(formatDate(date, 'MM-dd yyyy')).toEqual('11-04 2020');
+  expect(tEq(formatDate(date, 'MM-dd yyyy'))).toEqual(tEq('11-04 2020'));
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   const date = new Date('Wed Nov 4 2020 12:06:02 GMT+0800 (中国标准时间)')
-  expect(formatDate(date, 'hh:mm:ss dd/MM yyyy')).toEqual('12:06:02 04/11 2020');
+  expect(tEq(formatDate(date, 'hh:mm:ss dd/MM yyyy'))).toEqual(tEq('12:06:02 04/11 2020'));
 });
 
-test('object - objReverse', () => {
+test('date - formatDate', () => {
   const date = new Date('Wed Nov 4 2020 12:06:02 GMT+0800 (中国标准时间)')
-  expect(formatDate(date, 'h:m:s d-M-Y')).toEqual('12:6:2 4-11-2020');
+  expect(tEq(formatDate(date, 'h:m:s d-M-Y'))).toEqual(tEq('12:6:2 4-11-2020'));
 });
+
