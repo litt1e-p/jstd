5aef8a28c2ffc8d15bc58b52c313df26362c4c0e
litt1e-p <free9fw@gmail.com>
2022-04-13 17:21:36 +0800

    locale for sort

--- dist/types/lib/function/sort.d.ts
+++ dist/types/lib/function/sort.d.ts
@@ -3,6 +3,7 @@
  * @param a argv
  * @param b argv
  * @param ignoreCase ignore case or not, default is true
+ * @param locale locale for sorting, default is 'zh-Hans-CN'
  * @returns number
  */
-export declare const sort: (a: any, b: any, ignoreCase?: boolean) => any;
+export declare const sort: (a: any, b: any, ignoreCase?: boolean, locale?: string) => any;
